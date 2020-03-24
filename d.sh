338f68a891b893ef3055e4a6ec0478a531755006
Yan Hwee Yee <s10186981@connect.np.edu.sg>
2020-03-24 15:10:44 +0800

    Updated d.sh

--- d.sh
+++ d.sh
@@ -4,9 +4,7 @@ DISTRO="melodic"
 
 sudo apt-get install ros-$DISTRO-mavros ros-$DISTRO-mavros-extras -y
 
-wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
-
-sudo bash ./install_geographiclib_datasets.sh
+wget -O - https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh | sh
 
 # Install RQT package for ROS (optional)
 
