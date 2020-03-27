b8839ae3d97e4c74d7239517a6aade743710c0d7
Yan Hwee <yanhweeyee@gmail.com>
2020-03-27 19:19:26 +0800

    Removed old scripts

--- scripts/install.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-https://raw.githubusercontent.com/yanhwee/gazebo-ardupilot-ros/master/scripts/sudo_install.bash | sudo bash
-
-https://raw.githubusercontent.com/yanhwee/gazebo-ardupilot-ros/master/scripts/non_sudo_install.bash | bash
