5dc18fba6a680c53557b6ec1e2239a15da4eb2df
Yan Hwee <yanhweeyee@gmail.com>
2020-03-31 12:30:34 +0800

    Added misc.bash

---
+++ misc.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
+# Install Visual Studio Code
+sudo snap install --classic code
