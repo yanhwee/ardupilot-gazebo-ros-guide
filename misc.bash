a750b6fa5c5b6e049ca26934ecb400c32f5c8233
Yan Hwee <yanhweeyee@gmail.com>
2020-03-31 16:28:58 +0800

    Updated misc.bash: added -y

--- misc.bash
+++ misc.bash
@@ -4,7 +4,7 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update 
-sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-stable -y
 
 # Install Visual Studio Code
 sudo snap install --classic code
